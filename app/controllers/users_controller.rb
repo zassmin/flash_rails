ea53bfb037cccbab898f1f5fe533716d09ce75c9
dbcapprentice <dbcapprentice@dbc.com>
2013-08-05 18:55:54 -0700

    created users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
-  
+    def new
+      @user = User.new
+      render ''
+    end
+
+    def create
+      @user = User.new(user_name: params[:user_name], email: params[:email])
+      @user.password = params[:password]
+      @user.save
+      p start_session(@user)
+      redirect '/user_dashboard'
+    end
 
 end
