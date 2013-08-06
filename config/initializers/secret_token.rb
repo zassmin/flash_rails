57c88ce7347bde4fe65cac498e82a32d291c283a
dbcapprentice <dbcapprentice@dbc.com>
2013-08-05 18:23:31 -0700

    initial commit :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlashRails::Application.config.secret_token = 'a881cf1086b397aac17bed003c50c985e3db706a9c3a7da1e9c5be86fd5c98739ef484aa55378c7186ac8379e251da9e0b811638878ce160f87493d891783a22'
